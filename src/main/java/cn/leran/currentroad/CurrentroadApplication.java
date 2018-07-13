4f41ecc1c2f830fe0c544b183506fc321f9de793
邵益炯 <shaoyijiong@worken.cn>
2018-07-13 13:33:47 +0800

    Initial commit

---
+++ src/main/java/cn/leran/currentroad/CurrentroadApplication.java
@@ -0,0 +1,12 @@
+package cn.leran.currentroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentroadApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/leran/currentroad/CurrentroadApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.leran.currentroad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrentroadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
