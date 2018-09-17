ce4cfa436cbe03ee4dbbc2dba06794172d7c985a
邵益炯 <shaoyijiong@worken.cn>
2018-09-17 12:26:39 +0800

    LongAdder

--- src/main/java/cn/leran/currentroad/chapter4/Thread3.java
+++ src/main/java/cn/leran/currentroad/chapter4/Thread3.java
@@ -3,6 +3,7 @@ package cn.leran.currentroad.chapter4;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * AtomicInteger 通过cas技术,实现一个线程安全的int类型. 无锁策略
@@ -14,28 +15,36 @@ public class Thread3 {
 
   /**
    * 除了AtomicInteger java.util.concurrent.atomic 下还有各种类型的数据类型
-   * 高并发下 LongAdder 优于 AtomicLong
+   * 高并发下 LongAdder DoubleAdder 优于 AtomicLong
    */
   private static AtomicInteger atomicInteger = new AtomicInteger(0);
+  /**
+   * 初始值为0.
+   */
+  private static LongAdder longAdder = new LongAdder();
+
   private static Integer a = 0;
 
   /**
    * 测试.
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Runnable r = () -> {
-      atomicInteger.addAndGet(1);
+      //原子加一
+      atomicInteger.getAndIncrement();
+      longAdder.add(1);
       a = a + 1;
-      System.out.println("atomicInteger" + atomicInteger.get());
-      System.out.println("a:" + a);
     };
     ExecutorService pool = Executors.newFixedThreadPool(10000);
     for (int i = 0; i < 10000; i++) {
       pool.submit(r);
     }
-    //等待所有任务结束  AtomicInteger = 1000  Integer < 1000
+    //等待2s所有任务结束  AtomicInteger = 1000  Integer < 1000
     pool.shutdown();
+    Thread.sleep(2000);
+
     System.out.println(atomicInteger.get());
+    System.out.println(longAdder.toString());
     System.out.println(a);
   }
 }
