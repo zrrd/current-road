8bb28374a3fee7858ca3dfee30517a33a5c18419
邵益炯 <fantasy9417@163.com>
2019-08-09 09:45:08 +0800

    练习1  两个线程顺序打印

--- src/main/java/cn/leran/currentroad/chapter5/Test.java
+++ src/main/java/cn/leran/currentroad/chapter5/Test.java
@@ -1,56 +1,60 @@
 package cn.leran.currentroad.chapter5;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.IntStream;
-import org.springframework.util.StopWatch;
-
 /**
- * lambda Test.
- *
  * @author shaoyijiong
- * @date 2018/7/23
+ * @date 2019/8/9
  */
 public class Test {
 
-  public static class PrimeUntil {
 
-    public boolean isPrime(int num) {
-      int tmp = num;
-      if (tmp < 2) {
-        return false;
-      }
-      for (int i = 2; Math.sqrt(tmp) >= i; i++) {
-        if (tmp % i == 0) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
+  private static final Object object = new Object();
 
   /**
-   * lambda 中的并行操作
+   * 两个线程循环打印 直到10
    */
-  public static void main(String[] args) {
-    int[] num = {1, 2, 3, 4, 5, 6, 7};
-    Arrays.stream(num).forEach(System.out::println);
-    Random random = new Random();
-    //并行排序
-    Arrays.parallelSort(num);
-    //并行赋值
-    Arrays.parallelSetAll(num, (i) -> random.nextInt());
+  private static void test1() throws InterruptedException {
+    Runnable r1 = () -> {
+      int i = 0;
+      synchronized (object) {
+        while (i < 10) {
+          System.out.println(Thread.currentThread().getName() + ++i);
+          try {
+            // 唤醒其他线程去竞争 但是该线程还持有锁 其他线程无法进入同步块
+            object.notify();
+            // 释放锁 其他线程能够进入同步块
+            object.wait();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    };
 
-    StopWatch sw = new StopWatch();
-    sw.start();
-    long count1 = IntStream.range(1, 100000).filter(new PrimeUntil()::isPrime).count();
-    sw.stop();
-    System.out.println(count1 + "串行的时间" + sw.getLastTaskTimeMillis());
-    sw.start();
+    Runnable r2 = () -> {
+      int i = 0;
+      synchronized (object) {
+        while (i < 10) {
+          System.out.println(Thread.currentThread().getName() + ++i);
+          object.notify();
+          try {
+            object.wait();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    };
 
-    //加上parallel 将这个串行的改成并行的
-    long count2 = IntStream.range(1, 100000).parallel().filter(new PrimeUntil()::isPrime).count();
-    sw.stop();
-    System.out.println(count2 + "并行的时间" + sw.getLastTaskTimeMillis());
+    Thread t1 = new Thread(r1);
+    Thread t2 = new Thread(r2);
+    t1.start();
+    t2.start();
+    t1.join();
+    t2.join();
+  }
+
+
+  public static void main(String[] args) throws InterruptedException {
+    test1();
   }
 }
