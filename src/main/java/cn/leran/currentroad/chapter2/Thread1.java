b9fa1161554f778876e1e58ab645515cfe339f7d
邵益炯 <fantasy9417@163.com>
2019-09-12 14:09:08 +0800

    修改ReentrantLock 的使用方式

--- src/main/java/cn/leran/currentroad/chapter2/Thread1.java
+++ src/main/java/cn/leran/currentroad/chapter2/Thread1.java
@@ -1,5 +1,6 @@
 package cn.leran.currentroad.chapter2;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -8,7 +9,8 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author shaoyijiong
  * @date 2018/7/16
  */
-public class Thread1 implements Runnable {
+@SuppressWarnings("all")
+public class Thread1 {
 
   /**
    * <pre>
@@ -17,34 +19,69 @@ public class Thread1 implements Runnable {
    * </pre>
    */
   private static ReentrantLock lock = new ReentrantLock();
-  public static int i = 0;
-
-  @Override
-  public void run() {
-    for (int j = 0; j < 100000; j++) {
-      //lock.lockInterruptibly();
-      lock.lock();
-      //lock.lock(); 可以加多次锁  代码包装在try-finally代码块中来确保异常情况下的解锁非常重要
-      try {
-        i++;
-      } finally {
-        lock.unlock();
-        //lock.unlock(); 释放的时候也要释放相同次数的锁
-      }
-    }
-  }
+  public static volatile int i = 0;
 
   /**
    * 测试
    */
   public static void main(String[] args) throws InterruptedException {
-    Thread1 thread1 = new Thread1();
-    Thread t1 = new Thread(thread1);
-    Thread t2 = new Thread(thread1);
+
+    Thread t1 = new Thread(() -> {
+      System.out.println("t0 开始获取锁");
+      lock.lock();
+      System.out.println("t0 成功获取锁");
+      for (int j = 0; j < 1000; j++) {
+        i++;
+      }
+      try {
+        Thread.sleep(3_000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } finally {
+        lock.unlock();
+        System.out.println("t0 成功释放锁");
+      }
+    });
+
+    Thread t2 = new Thread(() -> {
+      System.out.println("t1 开始获取锁");
+      if (lock.tryLock()) {
+        System.out.println("t1 成功获取锁");
+        for (int j = 0; j < 1000; j++) {
+          i++;
+        }
+        lock.unlock();
+        System.out.println("t1 成功释放锁");
+      } else {
+        System.out.println("t1 获取锁失败");
+      }
+    });
+
+    Thread t3 = new Thread(() -> {
+      System.out.println("t2 开始获取锁");
+      try {
+        if (lock.tryLock(5, TimeUnit.SECONDS)) {
+          System.out.println("t2 成功获取锁");
+          for (int j = 0; j < 1000; j++) {
+            i++;
+          }
+          lock.unlock();
+          System.out.println("t2 成功释放锁");
+        } else {
+          System.out.println("t2 获取锁失败");
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
     t1.start();
     t2.start();
+    t3.start();
+
     t1.join();
     t2.join();
+    t3.join();
     System.out.println(i);
   }
 }
